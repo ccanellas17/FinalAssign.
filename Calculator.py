cd7688f470ee1c431f2925e6b043fd41637975c6
carlos <ccanellas17@gmail.com>
2018-09-13 14:56:01 +0200

    Simple calculator

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+print("1. Addition")
+print("2. Substraction")
+print("3. Multiplication")
+print("4. Division")
+
+command = input("Enter the desired comman: ")
+
+if command == 1:
+    n1 = int(input("Enter first number: "))
+    n2 = int(input("Enter second number: "))
+    print("Solution: ", n1+n2)
+
+if command == 2:
+    n1 = int(input("Enter first number: "))
+    n2 = int(input("Enter second number: "))
+    print("Solution: ", n1-n2)
+
+if command == 3:
+    n1 = int(input("Enter first number: "))
+    n2 = int(input("Enter second number: "))
+    print("Solution: ", n1*n2)
+
+if command == 4:
+    n1 = int(input("Enter first number: "))
+    n2 = int(input("Enter second number: "))
+    print("Solution: ", n1/n2)
